ad0d2d24fc7ee94e1229af1af47027c530d7c94a
Zheng Yang <zhengyangtng@gmail.com>
2026-01-20 14:38:47 +0800

    chnage cookie

--- risk_engine/device_cookie.py
+++ risk_engine/device_cookie.py
@@ -3,12 +3,14 @@ import hashlib
 from datetime import datetime, timedelta, timezone
 from sqlalchemy.orm import Session
 
+from sqlalchemy.exc import IntegrityError
+
 from risk_engine.db.cookie_model import DeviceToken
 
 
 COOKIE_NAME = "__Host_rba_dt"
 TOKEN_TTL_DAYS = 90
-
+ROTATE_IF_EXPIRES_WITHIN_DAYS = 7
 
 def sha256_hex(s: str) -> str:
     return hashlib.sha256(s.encode("utf-8")).hexdigest()
@@ -20,7 +22,27 @@ def utcnow() -> datetime:
 def generate_device_token(db: Session, user, device) -> tuple[str, datetime, str]:
     """
     Returns (raw_token, expires_at). Stores only hash in DB.
+
+    case 1st issue
+
+    case periodic rotate near expiry
+
+    case possible risk event rotation
+
     """
+
+    # check for active token
+
+    active = (
+        db.query(DeviceToken)
+        .filter(
+            DeviceToken.bound_user_id == user,
+            DeviceToken.bound_device_id == device,
+            DeviceToken.revoked == 0
+        )
+        .one_or_none()
+    )
+    print(active)
 
     # gen random token string and hash
     raw_token = secrets.token_urlsafe(32)
@@ -28,15 +50,26 @@ def generate_device_token(db: Session, user, device) -> tuple[str, datetime, str
 
     now = utcnow()
     exp = now + timedelta(days=TOKEN_TTL_DAYS)
+    try:
+        # revoke existing active token
+        db.query(DeviceToken).filter(
+            DeviceToken.bound_user_id == user,
+            DeviceToken.bound_device_id == device,
+            DeviceToken.revoked == 0
+        ).update({"revoked": 1}, synchronize_session=False)
 
-    db.add(DeviceToken(
-        token_hash=token_hash,
-        bound_device_id=device,
-        bound_user_id=user,
-        issued_at_utc=now,
-        expires_at_utc=exp,
-        revoked=0
-    ))
-    db.commit()
+        #
+        db.add(DeviceToken(
+            token_hash=token_hash,
+            bound_device_id=device,
+            bound_user_id=user,
+            issued_at_utc=now,
+            expires_at_utc=exp,
+            revoked=0
+        ))
+        db.commit()
 
-    return raw_token, exp , COOKIE_NAME
+    except IntegrityError:
+        db.rollback()
+
+    return raw_token, exp, COOKIE_NAME

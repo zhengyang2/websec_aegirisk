b5d76d70195802c6f0926330368683390dbfd3f3
Zheng Yang <zhengyangtng@gmail.com>
2026-01-22 17:33:33 +0800

    add extraction

--- web_app/WebApp/app/context_extract.py
+++ web_app/WebApp/app/context_extract.py
@@ -1,5 +1,9 @@
 
 from fastapi import Request
+from datetime import datetime, timezone
+
+
+
 
 def debug_print_request(request: Request):
     print("=== REQUEST DEBUG START ===")
@@ -31,6 +35,21 @@ def request_context_extract(request: Request):
     print(">>> request_context_extract CALLED <<<")
     debug_print_request(request)
 
+    ip = request.client.host
+
+    print(ip)
+
+    user_agent = request.headers.get("user-agent")
+
+    print(user_agent)
+
+    cookie_device_token = request.cookies.get("__Host_rba_dt")
+
+    print(cookie_device_token)
+
+    timestamp_utc = datetime.now(timezone.utc)
+
+    print(timestamp_utc)
 
     return
 
